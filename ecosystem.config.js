dde1104a3e32a3eeaa5b425847eca824c256bd0b
Ginés Sanz <gisanz2@alumni.uv.es>
2021-05-20 23:37:44 +0200

    logs pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,8 +6,9 @@ module.exports = {
     args : "run prod",
     time:true,
     log_date_format : "DD-MM-YYYY HH:mm:ss Z",
-    error_file: "/home/pi/serverpuertas/logs/rasp1_err.log",
-    out_file: "/home/pi/serverpuertas/logs/rasp1_out.log", 
+    error_file: "/home/pi/camarazero/logs/camara_err.log",
+    out_file: "/home/pi/camarazero/logs/camara_out.log", 
+    log_file: '/home/pi/camarazero/logs/camara_combined.log',
     watch: false,
     "watch_options": {
       "followSymlinks": false
